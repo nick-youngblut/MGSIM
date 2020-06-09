5b1273ba5a1672775e84b00b898709618c785c8e
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2020-06-09 08:52:54 +0200

    bumped the version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ install_reqs = [
 desc = 'Metagenome simulation of multiple synthetic communities'
 setup(
     name = 'MGSIM',
-    version = '0.1.12',
+    version = '0.1.13',
     description = desc,
     long_description = desc + '\n See README for more information.',
     author = 'Nick Youngblut',
