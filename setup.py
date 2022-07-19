bee5c46cd98964292eef616467f78c1f79b44ffc
Nick Youngblut <nyoungb2@gmail.com>
2022-07-19 13:59:22 +0000

    added .gitpod setup

--- setup.py
+++ setup.py
@@ -4,16 +4,6 @@ import os
 import glob
 import numpy
 
-    
-# dependencies
-install_reqs = [
-    'docopt>=0.6.2',
-    'numpy>=1.10',
-    'pandas>=0.18',
-    'scipy>=0.17',
-    'configobj>=5.0.6',
-    'biopython>=1.68'
-]
 
 ## install main application
 desc = 'Metagenome simulation of multiple synthetic communities'
@@ -29,7 +19,6 @@ setup(
             'MGSIM = MGSIM.__main__:main'
         ]
     },
-    install_requires = install_reqs,
     include_dirs = [numpy.get_include()],
     license = "MIT license",
     packages = find_packages(),
